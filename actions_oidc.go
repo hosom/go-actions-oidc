59d7a422169bdbc75005cca64e0dbd03e17748ee
hosom <hosom@github.com>
2025-05-21 13:53:54 -0400

    test run

--- actions_oidc.go
+++ actions_oidc.go
@@ -1,42 +1,101 @@
-package actions_oidc
+package main
 
 import (
-	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
 
-	"github.com/lestrrat-go/httprc/v3"
-	"github.com/lestrrat-go/jwx/v3/jwk"
-	"github.com/lestrrat-go/jwx/v3/jwt"
+	"github.com/MicahParks/keyfunc/v3"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 const (
 	GitHubWellKnownURL = "https://token.actions.githubusercontent.com/.well-known/jwks"
 )
 
+type ActionsClaims struct {
+	// RegisteredClaims are the standard claims required by the JWT spec
+	jwt.RegisteredClaims
+	Environment          string `json:"environment,omitempty"`
+	Ref                  string `json:"ref,omitempty"`
+	Sha                  string `json:"sha,omitempty"`
+	Repository           string `json:"repository,omitempty"`
+	RepositoryOwner      string `json:"repository_owner,omitempty"`
+	ActorID              string `json:"actor_id,omitempty"`
+	RepositoryVisibility string `json:"repository_visibility,omitempty"`
+	RepositoryID         string `json:"repository_id,omitempty"`
+	RepositoryOwnerID    string `json:"repository_owner_id,omitempty"`
+	RunID                string `json:"run_id,omitempty"`
+	RunNumber            string `json:"run_number,omitempty"`
+	RunAttempt           string `json:"run_attempt,omitempty"`
+	RunnerEnvironment    string `json:"runner_environment,omitempty"`
+	Actor                string `json:"actor,omitempty"`
+	Workflow             string `json:"workflow,omitempty"`
+	HeadRef              string `json:"head_ref,omitempty"`
+	BaseRef              string `json:"base_ref,omitempty"`
+	EventName            string `json:"event_name,omitempty"`
+	RefType              string `json:"ref_type,omitempty"`
+	JobWorkflowRef       string `json:"job_workflow_ref,omitempty"`
+}
+
+type TokenResponse struct {
+	Value string `json:"value,omitempty"`
+}
+
+func TokenRequest(aud string) (*http.Request, error) {
+	Url := os.Getenv("ACTIONS_ID_TOKEN_REQUEST_URL")
+	requestToken := os.Getenv("ACTIONS_ID_TOKEN_REQUEST_TOKEN")
+
+	req, err := http.NewRequest(http.MethodGet, Url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Authorization", "Bearer "+requestToken)
+	if aud != "" {
+		req.URL.Query().Set("audience", aud)
+	}
+	return req, nil
+}
+
 func main() {
 
-	ctx := context.Background()
-	cache, err := jwk.NewCache(ctx, httprc.NewClient())
+	req, err := TokenRequest("https://my-domain.com")
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
 
-	if err := cache.Register(ctx, GitHubWellKnownURL); err != nil {
-		return
-	}
-
-	keyset, err := cache.Lookup(ctx, GitHubWellKnownURL)
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
 
-	_ = keyset
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("Failed to get token: %s", resp.Status)
+	}
 
-	token := []byte(`eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c`)
-	parsed, err := jwt.Parse(token, jwt.WithKeySet(keyset))
+	var tokenResponse TokenResponse
+	if err := json.NewDecoder(resp.Body).Decode(&tokenResponse); err != nil {
+		log.Fatal(err)
+	}
+
+	k, err := keyfunc.NewDefault([]string{GitHubWellKnownURL})
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
 
-	// Do something with the parsed token
-	_ = parsed
+	tokenStr := tokenResponse.Value
+
+	token, err := jwt.ParseWithClaims(tokenStr, &ActionsClaims{}, k.Keyfunc)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if claims, ok := token.Claims.(*ActionsClaims); ok && token.Valid {
+		log.Printf("Token is valid. Claims: %+v\n", claims)
+	} else {
+		log.Println("Token is invalid.")
+	}
 }
--- go.mod
+++ go.mod
@@ -2,16 +2,10 @@ module actions-oidc
 
 go 1.24.3
 
-require github.com/lestrrat-go/jwx/v3 v3.0.1
+require github.com/MicahParks/keyfunc/v3 v3.3.11
 
 require (
-	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.4.0 // indirect
-	github.com/goccy/go-json v0.10.3 // indirect
-	github.com/lestrrat-go/blackmagic v1.0.3 // indirect
-	github.com/lestrrat-go/httpcc v1.0.1 // indirect
-	github.com/lestrrat-go/httprc/v3 v3.0.0-beta2 // indirect
-	github.com/lestrrat-go/option v1.0.1 // indirect
-	github.com/segmentio/asm v1.2.0 // indirect
-	golang.org/x/crypto v0.37.0 // indirect
-	golang.org/x/sys v0.32.0 // indirect
+	github.com/MicahParks/jwkset v0.8.0 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
+	golang.org/x/time v0.9.0 // indirect
 )
